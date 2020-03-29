cd9aa6b6e330f1009ee8da5a308cb8d7eeb9715f
momentjin <momentjin@gmail.com>
2020-03-29 18:13:37 +0900

    카카오 로그인 API 연동 1편 예제코드

---
+++ social-login/src/test/java/com/github/momentjin/sociallogin/SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.momentjin.sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
